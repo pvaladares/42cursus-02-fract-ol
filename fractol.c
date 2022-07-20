4ff5c49f419ca80d9920401ac3d19fd9f38fabcf
pvaladar <pvaladar@student.42lisboa.com>
2022-07-20 22:09:27 +0100

    1st working version

--- fractol.c
+++ fractol.c
@@ -6,12 +6,18 @@
 #include "fractol.h"
 #include <X11/keysym.h> // ESC key defintion
 #include <X11/Xlib.h> // XColor
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
 
 int deal_click(int pressed_click, void *param)
 {
     (void)param;
     (void)pressed_click;
     printf("click %d %c\n", pressed_click, pressed_click);
+    printf("%f\n", M_PI);
     return (0);
 }
 
@@ -28,7 +34,7 @@ int deal_key(int pressed_key, void *param)
 int continue_function(void *param)
 {
     (void)param;
-    puts("Hi");
+    puts("continue");
     return (0);
 }
 
@@ -37,7 +43,20 @@ int main(void)
     void    *mlx_ptr;
     void    *win_ptr;
     int     x, y;
+    //int     c;
+    //char    buffer;
+double MinRe = -2.0;
+double MaxRe = 1.0;
+double MinIm = -1.2;
+double MaxIm = MinIm+(MaxRe-MinRe)*HEIGHT/WIDTH;
+double Re_factor = (MaxRe-MinRe)/(WIDTH-1);
+double Im_factor = (MaxIm-MinIm)/(HEIGHT-1);
+unsigned MaxIterations = 100;
 
+    x = -2;
+    read(x, 0, 0);
+    //buffer = strerror(-1);
+    //puts(buffer);
     mlx_ptr = mlx_init();
     if (!mlx_ptr)
         return (1);
@@ -45,13 +64,36 @@ int main(void)
     if (!win_ptr)
         return (2);
     y = 0;
+    //c = HEIGHT;
     while (y < HEIGHT)
     {
+            double c_im = MaxIm - y*Im_factor;
+
         x = 0;
         while (x < WIDTH)
         {
-            if ((x - WIDTH / 2) * (x - WIDTH / 2) + (y - HEIGHT / 2) * (y - HEIGHT / 2) < 500)
-                mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0xFF0000);
+            double c_re = MinRe + x*Re_factor;
+
+        double Z_re = c_re, Z_im = c_im;
+        bool isInside = true;
+        for(unsigned n=0; n<MaxIterations; ++n)
+        {
+            double Z_re2 = Z_re*Z_re, Z_im2 = Z_im*Z_im;
+            if(Z_re2 + Z_im2 > 4)
+            {
+                isInside = false;
+                break;
+            }
+            Z_im = 2*Z_re*Z_im + c_im;
+            Z_re = Z_re2 - Z_im2 + c_re;
+        }
+        if(isInside) { mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0xFF0000); }
+
+            // z = 0 ; c = 0
+            // z_n+1 = zn * zn + c
+            //if ()
+            //if ((x - WIDTH / 2) * (x - WIDTH / 2) + (y - HEIGHT / 2) * (y - HEIGHT / 2) == 500)
+                
             x++;
         }
         y++;
