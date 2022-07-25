4d6995c4a01d3b5e3d7ff818005eaca11ab845e1
Pedro V <2190926+pvaladares@users.noreply.github.com>
2022-07-25 17:59:56 +0100

    Update fractol.c

--- src/fractol.c
+++ src/fractol.c
@@ -6,17 +6,16 @@
 /*   By: pvaladar <pvaladar@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 16:14:11 by pvaladar          #+#    #+#             */
-/*   Updated: 2022/07/25 13:55:43 by pvaladar         ###   ########.fr       */
+/*   Updated: 2022/07/25 17:42:43 by pvaladar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-typedef struct s_vars
-{
-	void	*mlx;
-	void	*win;
-}				t_vars;
+#include <time.h> // random rgb
+#include <stdlib.h> // random
+#include <unistd.h>
+
 
 /*
  This function closes the window and quits the program in a clean way
@@ -30,11 +29,16 @@ int	close_window_esc_key(int keycode, t_vars *vars)
 		mlx_destroy_display(vars->mlx);
 		exit(EXIT_SUCCESS);
 	}
-	else if (keycode == XK_Left || keycode == XK_Down)
-		puts("Zoom out");
-	else if (keycode == XK_Right || keycode == XK_Up)
-		puts("Zoom in");
-	printf("Pressed key %d %c\n", keycode, keycode);
+	else if (keycode == XK_Left)
+		puts("Left");
+	else if (keycode == XK_Right)
+		puts("Right");
+	else if (keycode == XK_Up)
+		puts("Up");
+	else if (keycode == XK_Down)
+		puts("Down");
+	else
+		printf("Pressed key %d %c\n", keycode, keycode);
 	return (0);
 }
 
@@ -64,11 +68,34 @@ int	close_window_cross_window(t_vars *vars)
 }
 
 /*
+  The bytes are not aligned, this means that the line_length differs from the actual window width.
+ We therefore should ALWAYS calculate the memory offset using the line length set by mlx_get_data_addr.
 
+We can calculate it very easily by using the following formula:
+*/
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+	int		offset;
+
+	offset = (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = data->addr + offset;
+	*(unsigned int*)dst = color;
+}
+
+/*
+ Create mlx instance
+ Create window instance
+ Create image instance
+ Get the image address and use custom put function to write without showing it
+ Render the image
 */
 int	main(int argc, char **argv)
 {
 	t_vars	vars;
+	t_data	img;
+	int		x;
+	int		y;
 
 	(void)(argv);
 	if (argc != 3)
@@ -84,16 +111,46 @@ int	main(int argc, char **argv)
 		perror("Error with mlx_init()");
 		exit(EXIT_FAILURE);
 	}
-	vars.win = mlx_new_window(vars.mlx, 500, 500, "Hello world!");
+	vars.win = mlx_new_window(vars.mlx, WIDTH, HEIGHT, "Hello world!");
 	if (!vars.win)
 	{
 		perror("Error with mlx_new_window()");
-		//free(vars.mlx);
 		exit(EXIT_FAILURE);
-	}	
-	mlx_hook(vars.win, KeyRelease, KeyReleaseMask, close_window_esc_key, &vars);
-	mlx_hook(vars.win, DestroyNotify, NoEventMask, close_window_cross_window, &vars);
-	mlx_hook(vars.win, ButtonPress, ButtonPressMask, mouse_hook, &vars);
+	}
+	img.img = mlx_new_image(vars.mlx, WIDTH, HEIGHT);
+	if (!img.img)
+	{
+		perror("Error with mlx_new_image()");
+		exit(EXIT_FAILURE);
+	}
+	img.addr = mlx_get_data_addr(
+		img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+
+
+
+	y = 0;
+	while (y < HEIGHT)
+	{
+					
+
+		x = 0;
+		while (x < WIDTH)
+		{
+			
+			//my_mlx_pixel_put(&img, x, y, 0x00FF0000);
+			my_mlx_pixel_put(&img, x, y, (x+y)*1000);
+			x++;
+		}
+		y++;
+	}
+
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+
+
+	
+	mlx_hook(vars.win, KeyRelease, KeyReleaseMask, close_window_esc_key, &vars); // ESC
+	mlx_hook(vars.win, DestroyNotify, StructureNotifyMask, close_window_cross_window, &vars); // X
+	mlx_hook(vars.win, ButtonPress, ButtonPressMask, mouse_hook, &vars); // Mouse scrool
 	mlx_loop(vars.mlx);
 	return (EXIT_SUCCESS);
 }
