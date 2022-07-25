69ab6cc93564fa9e10d84c1b8a9581ca1ee06ad6
Pedro V <2190926+pvaladares@users.noreply.github.com>
2022-07-25 17:59:41 +0100

    Update fractol.h

--- src/fractol.h
+++ src/fractol.h
@@ -6,31 +6,48 @@
 /*   By: pvaladar <pvaladar@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 16:13:36 by pvaladar          #+#    #+#             */
-/*   Updated: 2022/07/25 12:48:08 by pvaladar         ###   ########.fr       */
+/*   Updated: 2022/07/25 17:23:25 by pvaladar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// ======================== DEFINITIONS
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
 # define WIDTH		480
 # define HEIGHT		320
-//# define KEY_ESC	65307
 
-# include "mlx/mlx.h" // MLX
+// ======================== INCLUDES
 
-# include <X11/X.h> 
-/* Input Event Masks. Used as event-mask window attribute and as arguments
-   to Grab requests.  Not to be confused with event names.  */
-
-// Definition of X11 input event names (e.g.: KeyPress),
-/* Input Event Masks. Used as event-mask window attribute and as arguments
-   to Grab requests. */
-					// event masks (e.g.: KeyPressMask)
+# include "mlx/mlx.h" // local installation of MLX library
+# include <X11/X.h>
+/* For each event type, a corresponding constant name is defined in X11/X.h
+   More info about the relationship between Event Mask & Event Type here:
+   https://tronche.com/gui/x/xlib/events/processing-overview.html */
 # include <X11/keysym.h> // Mapping keyboard keys (e.g.: XK_Escape)
 # include <stdio.h> // perror()
 # include <stdlib.h> // exit()
 
+// ======================== TYPEDEF and STRUCTURES
+
+/*
+	mlx				: a pointer to the mlx instance
+	win				: a pointer to the window instance
+*/
+typedef struct s_vars
+{
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+/*
+	img				: a pointer to the image instance
+	addr			: a pointer to the memory address of the image
+	bits_per_pixel	: set by MiniLibX for the current data address
+	line_length		: set by MiniLibX for the current data address
+	endian			: set by MiniLibX for the current data address
+*/
 typedef struct s_data
 {
 	void	*img;
@@ -39,5 +56,7 @@ typedef struct s_data
 	int		line_length;
 	int		endian;
 }				t_data;
+
+// ======================== PROTOTYPES
 
 #endif
