6956713d190c9a34da30ba47fad269f57ef90eb8
Pedro artur Valadares de pinho <pvaladar@c2r9s3.42lisboa.com>
2022-07-25 13:57:08 +0100

    Mouse and key events working

--- src/fractol.c
+++ src/fractol.c
@@ -6,7 +6,7 @@
 /*   By: pvaladar <pvaladar@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 16:14:11 by pvaladar          #+#    #+#             */
-/*   Updated: 2022/07/24 00:02:14 by pvaladar         ###   ########.fr       */
+/*   Updated: 2022/07/25 13:55:43 by pvaladar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,72 @@ typedef struct s_vars
 	void	*win;
 }				t_vars;
 
-int	close(int keycode, t_vars *vars)
+/*
+ This function closes the window and quits the program in a clean way
+ whenever the ESC key is pressed
+*/
+int	close_window_esc_key(int keycode, t_vars *vars)
 {
-	//printf("Pressed key %d %c\n", keycode, keycode);
 	if (keycode == XK_Escape)
 	{
 		mlx_destroy_window(vars->mlx, vars->win);
+		mlx_destroy_display(vars->mlx);
 		exit(EXIT_SUCCESS);
 	}
+	else if (keycode == XK_Left || keycode == XK_Down)
+		puts("Zoom out");
+	else if (keycode == XK_Right || keycode == XK_Up)
+		puts("Zoom in");
+	printf("Pressed key %d %c\n", keycode, keycode);
 	return (0);
 }
 
-int	main(void)
+int	mouse_hook(int button, int x, int y, t_vars *vars)
+{
+	if (button == Button4)
+		puts("4 - Zoom in");
+	else if (button == Button5)
+		puts("5 - Zoom out");
+	(void)vars;
+	puts("Entered mouse hook");
+	printf("Coordinates x = %d , y = %d\n", x, y);
+	return (0);
+}
+
+/*
+ This function closes the window in a safe way whenever the cross on the window (X)
+ is pressed
+*/
+int	close_window_cross_window(t_vars *vars)
+{
+	(void)vars;
+	mlx_destroy_window(vars->mlx, vars->win);
+	mlx_destroy_display(vars->mlx);
+	exit(EXIT_SUCCESS);
+	return (0);
+}
+
+/*
+
+*/
+int	main(int argc, char **argv)
 {
 	t_vars	vars;
 
+	(void)(argv);
+	if (argc != 3)
+	{
+		printf("\e[31m## error - incorrect syntax ##\n\e[0m");
+		printf(
+			"\e[92m./fractol <fractal-number | fractal-name or> [<optional parameters>]\n\e[0m");
+		return (EXIT_FAILURE);
+	}
 	vars.mlx = mlx_init();
 	if (!vars.mlx)
 	{
 		perror("Error with mlx_init()");
 		exit(EXIT_FAILURE);
-	}	
+	}
 	vars.win = mlx_new_window(vars.mlx, 500, 500, "Hello world!");
 	if (!vars.win)
 	{
@@ -46,7 +91,9 @@ int	main(void)
 		//free(vars.mlx);
 		exit(EXIT_FAILURE);
 	}	
-	mlx_hook(vars.win, KeyPress, KeyPressMask, close, &vars);
+	mlx_hook(vars.win, KeyRelease, KeyReleaseMask, close_window_esc_key, &vars);
+	mlx_hook(vars.win, DestroyNotify, NoEventMask, close_window_cross_window, &vars);
+	mlx_hook(vars.win, ButtonPress, ButtonPressMask, mouse_hook, &vars);
 	mlx_loop(vars.mlx);
 	return (EXIT_SUCCESS);
 }
--- src/fractol.h
+++ src/fractol.h
@@ -6,7 +6,7 @@
 /*   By: pvaladar <pvaladar@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 16:13:36 by pvaladar          #+#    #+#             */
-/*   Updated: 2022/07/23 23:47:06 by pvaladar         ###   ########.fr       */
+/*   Updated: 2022/07/25 12:48:08 by pvaladar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,17 @@
 # define HEIGHT		320
 //# define KEY_ESC	65307
 
-# include <mlx.h>
-# include <X11/X.h> // Definition of X11 input event masks, event names
-# include <X11/keysym.h> // Mapping keyboard keys (e.g.: ESC)
+# include "mlx/mlx.h" // MLX
+
+# include <X11/X.h> 
+/* Input Event Masks. Used as event-mask window attribute and as arguments
+   to Grab requests.  Not to be confused with event names.  */
+
+// Definition of X11 input event names (e.g.: KeyPress),
+/* Input Event Masks. Used as event-mask window attribute and as arguments
+   to Grab requests. */
+					// event masks (e.g.: KeyPressMask)
+# include <X11/keysym.h> // Mapping keyboard keys (e.g.: XK_Escape)
 # include <stdio.h> // perror()
 # include <stdlib.h> // exit()
 
